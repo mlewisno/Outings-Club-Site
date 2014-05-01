f63dcf9a1ca301000b4eac3dca362b762bdf1e73
mlewisno <mlewisno@gmail.com>
2014-04-30 21:22:45 -0400

    Made changes

--- menu_gen.js
+++ menu_gen.js
@@ -2,15 +2,31 @@ $(document).ready(function() {
     
     /* Every time the window is scrolled ... */
     $(window).scroll( function(){
+        
+        var faded_in = 0;
     
-        /* Check the location of each desired element */
-        $('.divider_icon').each( function(i){
-            
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        /* Check the location of the nav menu in comparison to the window */
+        $('#nav_menu_wrapper').each( function(i){
+            var fade_zone = $('#content').position().top - 2*$(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            
+            /* If the object is completely visible in the window, fade it in */
+            if( bottom_of_window > fade_zone && !faded_in){
+                $(this).animate({'opacity':'1'},3000);
+                $( "#nav_menu_wrapper" ).attr("position", "fixed");
+                faded_in = 1;
+            }
+            
+        }); 
+        
+        /* Check the location of each of the icons */
+        $('.icons').each( function(i){
+            var icon_bottom = $(this).position().top;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* Fades is all the icons of this class */
+            if( bottom_of_window > icon_bottom){
                 $(this).animate({'opacity':'1'},3000);
             }
             
